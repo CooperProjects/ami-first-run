77a46baead8560c3d3a908656f300c22f6823014
pachanga <fatin.d.cooper@students.coookman.edu>
2017-07-05 06:05:39 +0000

    first commit

---
+++ gitit.sh
@@ -0,0 +1,7 @@
+
+#echo "# ami-first-run" >> README.md
+#git init
+git add .
+git commit -m "first commit"
+git remote add origin git@github.com:CooperProjects/ami-first-run.git
+git push -u origin master
